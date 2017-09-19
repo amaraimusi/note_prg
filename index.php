1ce1178431e247fc0666387da51e9dc5516925e2
k_uehara <amaraimusi@gmail.com>
2017-09-19 11:11:44 +0900

    Update knowledge

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="google" content="notranslate" />
+		<title>ワクガンス | プログラミングの覚書</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"  />
+		<link rel="stylesheet" type="text/css" href="css/common2.css"  />
+		<script>
+
+
+
+		</script>
+
+		<style type="text/css">
+
+			#list1  li{
+				margin-top:16px;
+				margin-bottom:6px;
+				margin-left:20px;
+				font-size:1.5em;
+				
+			}
+
+			#list1  li a{
+				color:#18548d;
+				font-size:1.6em;
+			}
+
+			#list1  li a:hover{
+				color:#de5925;
+			}
+		</style>
+
+	</head>
+	<body>
+		<div id="header" ><h1>ワクガンス | プログラミングの覚書</h1></div>
+		<div class="container">
+			
+			<ul id="list1">
+				<li><a href="mysql">MySQLの覚書</a></li>
+				<li><a href="JavaScript">JavaScriptの覚書</a></li>
+				<li><a href="php">PHPの覚書</a></li>
+				<li><a href="cakephp">CakePHPの覚書</a></li>
+				<li><a href="gmap">Google Maps APIの覚書</a></li>
+				<li><a href="html">HTMLの覚書</a></li>
+				<li><a href="sqlite">SQLiteの覚書</a></li>
+				<li><a href="css_note">CSSの覚書</a></li>
+				<li><a href="node_js">Node.jsの覚書</a></li>
+				<li><a href="eclipse">Eclipseの覚書</a></li>
+				<li><a href="cpp">C++の覚書</a></li>
+				<li><a href="android">Androidの覚書</a></li>
+				<li><a href="angular">Aungularの覚書</a></li>
+				<li><a href="it">情報技術</a></li>
+				<li><a href="other">その他</a></li>
+				<li><a href="travel">旅記録</a></li>
+				<li><a href="nogyo_note">農業の覚書</a></li>
+			</ul>
+		
+			
+		</div><!-- content -->
+		<div id="footer">(C) kenji uehara 2014/4/24</div>
+	</body>
+</html>
