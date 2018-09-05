99279d3548751d80f1d62d729759ef6989c4f8fe
user <user@user-PC>
2018-09-05 15:05:53 +0900

    MySQLのコマンドを動かす | SHELL

--- mysql/index.php
+++ mysql/index.php
@@ -98,6 +98,7 @@
 <ol>
 	<li><a href="cmd_note.html#sec1-1" >MySQLのコマンドを動かす | ローカル</a></li>
 	<li><a href="cmd_note.html#sec1-1-2" >MySQLのコマンドを動かす | Git Bash</a></li>
+	<li><a href="cmd_note.html#sec1-1-3" >MySQLのコマンドを動かす | SHELL</a></li>
 	<li><a href="cmd_note.html#sec1-2" >MySQLをコマンドで動かす | サーバー（さくらサーバー）</a></li>
 	<li><a href="cmd_note.html#sec1-3" >DBをバックアップ（エクスポート）する | mysqldump </a></li>
 	<li><a href="cmd_note.html#sec1-4" >リモートでバックアップする（さくらサーバー） </a></li>
