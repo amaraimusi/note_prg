b10340e55ec8b1cbae8bcae563c160582051c40d
user <amaraimusi@gmail.com>
2021-12-22 18:09:13 +0900

    SQLite

--- sqlite/index.php
+++ sqlite/index.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
 <html lang="ja">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="google" content="notranslate" />
-		<title>SQLiteの覚書</title><link rel='shortcut icon' href='/home/images/favicon.ico' />
-		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css"  />
-		<link rel="stylesheet" type="text/css" href="../css/common2.css"  />
-		<script>
+<head>
+	<meta charset="UTF-8">
+	<meta name="google" content="notranslate" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SQLiteの覚書 | ワクガンス</title>
+	<link rel='shortcut icon' href='/home/images/favicon.ico' />
+	
+	<link href="/note_prg/css/bootstrap-4.3.1-dist/bootstrap.min.css" rel="stylesheet">
+	<link href="/note_prg/css/bootstrap-4.3.1-dist/font/css/open-iconic.min.css" rel="stylesheet">
+	<link href="/note_prg/css/highlight/default.css" rel="stylesheet">
+	<link href="/note_prg/css/common2.css" rel="stylesheet">
+	<script src="/note_prg/js/jquery3.js"></script>	<!-- jquery-3.3.1.min.js -->
+	<script src="/note_prg/js/bootstrap-4.3.1-dist/bootstrap.bundle.min.js"></script>
+	<script src="/note_prg/js/vue.min.js"></script>
+	<script src="/note_prg/js/highlight.pack.js"></script>
+	<script src="/note_prg/js/livipage.js"></script>
+	<script src="/note_prg/js/ImgCompactK.js"></script>
 
+	<script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+<div id="header" ><h1>SQLiteの覚書 | ワクガンス</h1></div>
+<div class="container">
 
+<h2>目次</h2>
+<ol>
+	<li>SQLite3の覚書</li>
+	<li><a href="#v1_index">SQLite バージョン1</a></li>
+</ol>
 
-		</script>
+<h2>SQLite3の覚書</h2>
+<ol>
+	<li><a href="sqlite3_note.html#sec1-1">Windows10にSQLiteをインストール→テーブル作成まで</a></li>
+</ol>
+<hr />
 
-		<style type="text/css">
-
-		</style>
-
-	</head>
-	<body>
-		<div id="page1">
-		<h1 id="header">SQLiteの覚書</h1>
-		<div id="content" >
-
+<h2 id="v1_index">SQLite バージョン1</h2>
 			<table border="1">
 				<thead>
 					<tr><th>タイトル</th><th>カテゴリ</th></tr>
@@ -38,12 +54,17 @@
 					<tr><td><a href='note.php#a9' >FireFoxのアドイン | SQLite Manager</a></td><td>SQLite Manager</td></tr>
 					<tr><td><a href='note.php#a10' >Android SDKでdbファイルを取得する手順</a></td><td>Android</td></tr>
 
-
-
 				</tbody>
 			</table>
-		</div><!-- content -->
-		<div id="footer">(C) kenji uehara 2014/7/7</div>
-		</div><!-- page1 -->
-	</body>
+			
+<div class="yohaku"></div>
+<ol class="breadcrumb">
+	<li><a href="/">ホーム</a></li>
+	<li><a href="/note_prg">プログラミングの覚書</a></li>
+	<li>SQLiteの覚書</li>
+</ol>
+
+</div><!-- content -->
+<div id="footer">(C) kenji uehara 2014-7-7 | 2021-12-22</div>
+</body>
 </html>
