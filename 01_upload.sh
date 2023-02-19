f5d105632551f9022ba3ccdaa146be073b015151
amaraimusi <amaraimusi@gmail.com>
2023-02-20 08:37:39 +0900

    とりえあえずコミット

--- 01_upload.sh
+++ 01_upload.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/sh
 echo 'ソースコードを差分アップロードします。'
 
-rsync -auvz ../note_prg amaraimusi@amaraimusi.sakura.ne.jp:www
+rsync -auvz --exclude='.git/' ../note_prg amaraimusi@amaraimusi.sakura.ne.jp:www
 
 
 echo "------------ 送信完了"
